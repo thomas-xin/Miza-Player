3646949df6e343510415ce0d80724781159445b8
Thomas Xin <thomasxin@gmail.com>
2022-06-23 00:27:05 +1000

    typo x2

--- misc/install_update_p.py
+++ misc/install_update_p.py
@@ -105,7 +105,7 @@ try:
 	assert v >= "2022.3.8.1"
 except:
 	traceback.print_exc()
-	subprocess.run([python, "-m", "pip", "install", "git+https://github.com/yt-dlp/yt-dlp.git", "--upgrade", "--user"])
+	subprocess.run([sys.executable, "-m", "pip", "install", "git+https://github.com/yt-dlp/yt-dlp.git", "--upgrade", "--user"])
 
 # if os.name == "nt":
 	# for k, v in (

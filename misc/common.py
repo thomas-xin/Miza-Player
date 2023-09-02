9487f8ecb539b1d73bfd95ccc63d8b117c6a57c4
Thomas <thomasxin@gmail.com>
2023-09-03 02:39:15 +1000

    remove subpixel, it seems to make it worse here

--- misc/common.py
+++ misc/common.py
@@ -1315,7 +1315,7 @@ def start_display():
 			else:
 				raise NotImplementedError(flags)
 			if not sp:
-				sp = pyglet.sprite.Sprite(im, blend_src=blend_src, blend_dest=blend_dest, batch=batch, group=group, subpixel=True)
+				sp = pyglet.sprite.Sprite(im, blend_src=blend_src, blend_dest=blend_dest, batch=batch, group=group)
 				globals()["NEW_SPRITES"].append(sp)
 			sp.flags = flags
 			if not redraw and not permanent and not (area and different):
